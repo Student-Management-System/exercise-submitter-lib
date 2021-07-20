de7eaf7d1d56f0a5a67710c23dfd33264b628ec0
Lukas Dönges <lukasdoenges@t-online.de>
2021-07-20 21:29:43 +0200

    added Copy Files and Test Case

--- src/main/java/net/ssehub/teaching/exercise_submitter/lib/submission/Preparator.java
+++ src/main/java/net/ssehub/teaching/exercise_submitter/lib/submission/Preparator.java
@@ -3,35 +3,53 @@ package net.ssehub.teaching.exercise_submitter.lib.submission;
 import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-class Preparator implements Closeable{
-    
+class Preparator implements Closeable {
+
     private File result;
 
     public Preparator(File directory) throws IOException {
-        
-        if(!directory.isDirectory()) {
+
+        if (!directory.isDirectory()) {
             throw new IOException(directory.getName() + " is not a directory");
-            
+
         }
-        
-        result = File.createTempFile("exercise_submission", null);
-        result.delete();
-        result.mkdir();
-        result.deleteOnExit();
-        
-       
-      
+
+        this.result = File.createTempFile("exercise_submission", null);
+        this.result.delete();
+        this.result.mkdir();
+        Preparator.copyDirectory(directory.getAbsolutePath(), this.result.getAbsolutePath());
+
+        this.result.deleteOnExit();
+
     }
-    
+
     public File getResult() {
-        return result;
+        return this.result;
     }
 
     @Override
     public void close() throws IOException {
-        result.delete();
-        
+        this.result.delete();
+
     }
-    
+
+    private static void copyDirectory(String sourceDirectoryLocation, String destinationDirectoryLocation)
+            throws IOException {
+        Files.walk(Paths.get(sourceDirectoryLocation)).forEach(source -> {
+            Path destination = Paths.get(destinationDirectoryLocation,
+                    source.toString().substring(sourceDirectoryLocation.length()));
+
+            try {
+                Files.copy(source, destination);
+            } catch (IOException e) {
+
+            }
+
+        });
+    }
+
 }
--- src/test/java/net/ssehub/teaching/exercise_submitter/lib/submission/PreparatorTest.java
+++ src/test/java/net/ssehub/teaching/exercise_submitter/lib/submission/PreparatorTest.java
@@ -9,58 +9,120 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
 
 public class PreparatorTest {
-    
-    private final static File TESTDATA = new File("src/test/resources/PreparatorTest"); 
-    
+
+    private final static File TESTDATA = new File("src/test/resources/PreparatorTest");
+
     @Test
     public void throwsIfDirDoesntExist() {
         IOException ex = assertThrows(IOException.class, () -> new Preparator(new File("doesnt_exist")));
         assertEquals("doesnt_exist is not a directory", ex.getMessage());
     }
-    
+
     @Test
     public void throwsIfDirIsFile() {
         IOException ex = assertThrows(IOException.class, () -> new Preparator(new File(TESTDATA, "file.txt")));
         assertEquals("file.txt is not a directory", ex.getMessage());
     }
-    
+
     @Test
     public void prepareEmptyFolder() {
         File source = new File(TESTDATA, "emptyDir");
         source.mkdir();
         assertTrue(source.isDirectory(), "precondition: empty test directory exists");
-        
-       assertDoesNotThrow(() -> { 
-           try (Preparator preparator = new Preparator(source)) {
+
+        assertDoesNotThrow(() -> {
+            try (Preparator preparator = new Preparator(source)) {
                 File result = preparator.getResult();
-        
+
                 assertTrue(result.isDirectory());
                 assertEquals(0, result.listFiles().length);
                 assertNotEquals(result, source);
             }
-       });
+        });
     }
-    
+
     @Test
     public void clearsCopiedEmptyDir() {
         File source = new File(TESTDATA, "emptyDir");
         source.mkdir();
         assertTrue(source.isDirectory(), "precondition: empty test directory exists");
-        
+
         assertDoesNotThrow(() -> {
             File result;
             try (Preparator preparator = new Preparator(source)) {
-        
+
                 result = preparator.getResult();
                 assertTrue(result.isDirectory());
             }
-            
+
             assertFalse(result.exists());
         });
-    
+
     }
+
+    @Test
+    public void copiedFilesinEmptyDir() {
+        File source = new File(TESTDATA, "notEmptyDir");
+        assertTrue(source.isDirectory(), "precondition: empty test directory exists");
+
+        assertDoesNotThrow(() -> {
+            File result;
+            try (Preparator preparator = new Preparator(source)) {
+
+                result = preparator.getResult();
+                assertTrue(result.isDirectory());
+            }
+
+            assertFalse(result.list() == null);
+        });
+
+    }
+
+    @Test
+    public void copiedAllFilesAndSubDirsinEmptyDir() {
+        File source = new File(TESTDATA, "notEmptyDirWithSubDir");
+        assertTrue(source.isDirectory(), "precondition: empty test directory exists");
+
+        assertDoesNotThrow(() -> {
+            File result;
+            try (Preparator preparator = new Preparator(source)) {
+
+                result = preparator.getResult();
+                assertTrue(result.isDirectory());
+
+                URI SourceDirPath = source.toURI();
+                URI ResultDirPath = result.toURI();
+
+                for (File file : source.listFiles()) {
+
+                    URI sourcePath = file.toURI();
+                    URI relativeSourcePath = SourceDirPath.relativize(sourcePath);
+
+                    Files.walk(Paths.get(result.getAbsolutePath())).forEach(otherfile -> {
+                        boolean first = false;
+
+                        URI resultPath = otherfile.toUri();
+                        URI relativeResultPath = ResultDirPath.relativize(resultPath);
+
+                        if (relativeResultPath != relativeSourcePath && first != false) {
+                            assertTrue(false);
+                        }
+                        first = true;
+                    });
+
+                }
+
+            }
+
+        });
+
+    }
+
 }
