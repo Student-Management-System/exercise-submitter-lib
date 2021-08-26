b3d5d6d70cf4fa4c8094ddbe250bae01b04eaa37
Lukas Dönges <lukasdoenges@t-online.de>
2021-08-26 22:21:30 +0200

    Added Submitter + SubmitterIT + resources

--- src/main/java/net/ssehub/teaching/exercise_submitter/lib/submission/Submitter.java
+++ src/main/java/net/ssehub/teaching/exercise_submitter/lib/submission/Submitter.java
@@ -2,93 +2,174 @@ package net.ssehub.teaching.exercise_submitter.lib.submission;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import net.ssehub.teaching.exercise_submitter.lib.submission.Problem.Severity;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.tmatesoft.svn.core.SVNAuthenticationException;
+import org.tmatesoft.svn.core.SVNCommitInfo;
+import org.tmatesoft.svn.core.SVNDepth;
+import org.tmatesoft.svn.core.SVNErrorCode;
+import org.tmatesoft.svn.core.SVNErrorMessage;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.auth.BasicAuthenticationManager;
+import org.tmatesoft.svn.core.wc.DefaultSVNCommitParameters;
+import org.tmatesoft.svn.core.wc.SVNClientManager;
+import org.tmatesoft.svn.core.wc.SVNCommitClient;
+import org.tmatesoft.svn.core.wc.SVNRevision;
+import org.tmatesoft.svn.core.wc.SVNStatusType;
+import org.tmatesoft.svn.core.wc.SVNUpdateClient;
+import org.tmatesoft.svn.core.wc.SVNWCClient;
+import org.xml.sax.SAXException;
+
+import net.ssehub.teaching.exercise_submitter.lib.ExerciseSubmitterManager;
+import net.ssehub.teaching.exercise_submitter.lib.student_management_system.AuthenticationException;
 
 /**
  * Submits solutions to a given SVN exercise submission location.
- * 
+ *
  * @author Adam
  */
 public class Submitter {
 
+    private SVNClientManager clientmanager;
+    private SVNCommitClient commitclient;
+
     private String url;
-    
+    private ExerciseSubmitterManager.Credentials cred;
+
     /**
      * Creates a new submitter for the given SVN location.
-     * 
-     * @param url The URL of the homework folder that should be submitted to.
+     *
+     * @param url  The URL of the homework folder that should be submitted to.
+     * @param cred the credentials
      */
-    public Submitter(String url) {
+    public Submitter(String url, ExerciseSubmitterManager.Credentials cred) {
         this.url = url;
+        this.cred = cred;
+
     }
-    
+
     /**
      * Submits the given directory.
-     * 
+     *
      * @param directory The directory that contains the solution to be submitted.
-     * 
+     *
      * @return The result of the submission.
-     * 
-     * @throws SubmissionException If the submission fails.
+     *
+     * @throws SubmissionException      If the submission fails.
      * @throws IllegalArgumentException If the given directory is not a directory.
+     * @throws AuthenticationException
      */
     public SubmissionResult submit(File directory)
-            throws SubmissionException, IllegalArgumentException {
-        
+            throws SubmissionException, IllegalArgumentException, AuthenticationException {
+
         if (!directory.isDirectory()) {
             throw new IllegalArgumentException(directory + " is not a directory");
         }
-        
-        try {
-            Thread.sleep(1500);
-        } catch (InterruptedException e) {
-        }
-        
-        Optional<File> randomJavaFile;
-        try {
-            randomJavaFile = Files.walk(directory.toPath())
-                    .filter(Files::isRegularFile)
-                    .map(p -> directory.toPath().relativize(p))
-                    .map(Path::toFile)
-                    .filter(f -> f.getName().endsWith(".java"))
-                    .findFirst();
+        SubmissionResult submissionresult = null;
+
+        try (Preparator preparator = new Preparator(directory)) {
+
+            File result = preparator.getResult();
+
+            this.clientmanager = SVNClientManager.newInstance(null,
+                    BasicAuthenticationManager.newInstance(this.cred.getUsername(), this.cred.getPassword()));
+
+            this.commitclient = this.clientmanager.getCommitClient();
+
+            this.setCommitParameters();
+
+            SVNURL svnurl = SVNURL.parseURIEncoded(this.url);
+
+            this.doCheckout(result, svnurl);
+
+            SVNCommitInfo info = null;
+
+            try {
+                info = this.commitclient.doCommit(new File[] {result}, false, "Testcommit", null, null, false, false,
+                        SVNDepth.INFINITY);
+
+            } catch (SVNAuthenticationException e) {
+                throw new AuthenticationException("SVN cant authenticate succesfully");
+
+            } catch (SVNException e) {
+                SVNErrorMessage errorMsg = e.getErrorMessage();
+                if (errorMsg.hasChildWithErrorCode(SVNErrorCode.REPOS_HOOK_FAILURE)) {
+                    SvnResultHandler handler = new SvnResultHandler(SvnResultHandler.svnErrorMessageToString(errorMsg));
+                    submissionresult = new SubmissionResult(false, handler.parseXmlToProblem());
+                } else {
+                    throw new SubmissionException("Response parse Error");
+                }
+            }
+
+            if (info.getErrorMessage() != null) {
+                SvnResultHandler handler = new SvnResultHandler(
+                        SvnResultHandler.svnErrorMessageToString(info.getErrorMessage()));
+                submissionresult = new SubmissionResult(true, handler.parseXmlToProblem());
+            } else {
+                List<Problem> emptylist = new ArrayList<Problem>();
+                if (info.getNewRevision() == -1) {
+
+                    submissionresult = new SubmissionResult(true, emptylist);
+                } else {
+                    submissionresult = new SubmissionResult(true, emptylist);
+                }
+            }
+
         } catch (IOException e) {
-            throw new SubmissionException(e);
+            throw new SubmissionException("cant create temp dir", e);
+        } catch (SVNException | ParserConfigurationException | SAXException e) {
+            throw new SubmissionException("cant do checkout", e);
         }
-        
-        SubmissionResult result;
-        
-        if (randomJavaFile.isPresent()) {
-            List<Problem> problems = new LinkedList<>();
-            
-            Problem p1 = new Problem("javac", "Missing ';'", Severity.ERROR);
-            p1.setFile(randomJavaFile.get());
-            p1.setLine(5);
-            p1.setColumn(10);
-            
-            Problem p2 = new Problem("checkstyle", "Some checkstyle message", Severity.WARNING);
-            p2.setFile(randomJavaFile.get());
-            p2.setLine(10);
-            
-            problems.add(p1);
-            problems.add(p2);
-            
-            result = new SubmissionResult(true, new LinkedList<>(problems));
-            
-        } else {
-            Problem p1 = new Problem("javac", "No Java files found", Severity.ERROR);
-            
-            result = new SubmissionResult(false, Arrays.asList(p1));
-        }
-        
-        return result;
+        return submissionresult;
+
     }
-    
+
+    /**
+     * Sets the commit parameters which are needed for the checkout.
+     */
+    private void setCommitParameters() {
+        this.commitclient.setCommitParameters(new DefaultSVNCommitParameters() {
+            @Override
+            public Action onMissingFile(File file) {
+                return DELETE;
+            }
+
+            @Override
+            public Action onMissingDirectory(File file) {
+                return DELETE;
+            }
+        });
+    }
+
+    /**
+     * Does a Svn checkout for the projekt files in the directory from the
+     * repository behind the url.
+     *
+     * @param dir    dir for the checkout
+     * @param svnurl the svnurl where the repository is located
+     * @throws SVNException the SVN exception
+     */
+    private void doCheckout(File dir, SVNURL svnurl) throws SVNException {
+        SVNUpdateClient update = this.clientmanager.getUpdateClient();
+        update.doCheckout(svnurl, dir, SVNRevision.HEAD, SVNRevision.HEAD, SVNDepth.INFINITY, true);
+
+        SVNWCClient wcClient = this.clientmanager.getWCClient();
+
+        this.clientmanager.getStatusClient().doStatus(dir, SVNRevision.HEAD, SVNDepth.INFINITY, false, false, false,
+                false, status -> {
+                SVNStatusType type = status.getNodeStatus();
+                File file = status.getFile();
+
+                if (type == SVNStatusType.STATUS_UNVERSIONED) {
+                    wcClient.doAdd(file, true, false, false, SVNDepth.EMPTY, false, false);
+
+                } else if (type == SVNStatusType.STATUS_MISSING) {
+                    wcClient.doDelete(file, true, false, false);
+                }
+            }, null);
+    }
 }
---
+++ src/test/java/net/ssehub/teaching/exercise_submitter/lib/submission/SubmitterIT.java
@@ -0,0 +1,105 @@
+package net.ssehub.teaching.exercise_submitter.lib.submission;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import net.ssehub.studentmgmt.docker.StuMgmtDocker;
+import net.ssehub.studentmgmt.docker.StuMgmtDocker.AssignmentState;
+import net.ssehub.studentmgmt.docker.StuMgmtDocker.Collaboration;
+import net.ssehub.teaching.exercise_submitter.lib.ExerciseSubmitterFactory;
+import net.ssehub.teaching.exercise_submitter.lib.ExerciseSubmitterManager;
+import net.ssehub.teaching.exercise_submitter.lib.data.Assignment;
+
+
+public class SubmitterIT {
+
+    private static StuMgmtDocker docker;
+
+    private final static File TESTDATA = new File("src/test/resources/SubmitterTest");
+    
+    private static String homework02id = null;
+
+    @BeforeAll
+    public static void setupServers() {
+        docker = new StuMgmtDocker();
+        docker.createUser("svn", "abcdefgh");
+        docker.createUser("adam", "123456");
+        docker.createUser("student1", "123456");
+        docker.createUser("student2", "123456");
+        docker.createUser("student3", "123456");
+        docker.createUser("student4", "123456");
+
+        String courseId = docker.createCourse("java", "wise2021", "Programmierpraktikum: Java", "adam", "svn");
+
+        docker.enrollStudent(courseId, "student1");
+        docker.enrollStudent(courseId, "student2");
+        docker.enrollStudent(courseId, "student3");
+        docker.enrollStudent(courseId, "student4");
+
+        docker.createGroup(courseId, "JP001", "student1", "student3");
+        docker.createGroup(courseId, "JP002", "student2", "student4");
+
+        String a1 = docker.createAssignment(courseId, "Homework01", AssignmentState.INVISIBLE, Collaboration.GROUP);
+        String a2 = docker.createAssignment(courseId, "Homework02", AssignmentState.INVISIBLE, Collaboration.GROUP);
+        docker.createAssignment(courseId, "Testat01", AssignmentState.INVISIBLE, Collaboration.SINGLE);
+
+        docker.changeAssignmentState(courseId, a1, AssignmentState.SUBMISSION);
+        docker.changeAssignmentState(courseId, a1, AssignmentState.IN_REVIEW);
+
+        // start the SVN late, so that only one assignment change event triggers a full
+        // update
+        docker.startSvn(courseId, "svn");
+
+        docker.changeAssignmentState(courseId, a2, AssignmentState.SUBMISSION);
+        
+        homework02id = a2;
+    }
+
+    @AfterAll
+    public static void tearDownServers() {
+        docker.close();
+    }
+    
+    @Test 
+    public void submitTest() {
+        assertDoesNotThrow(() -> {
+            
+        Preparator prep = new Preparator(new File(TESTDATA, "Works"));
+        
+        File dir = prep.getResult();
+        
+        ExerciseSubmitterFactory fackto = new ExerciseSubmitterFactory();
+        fackto.withAuthUrl(docker.getAuthUrl());
+        fackto.withMgmtUrl(docker.getStuMgmtUrl());
+        fackto.withSvnUrl(docker.getSvnUrl());
+        fackto.withUsername("student1");
+        fackto.withPassword("123456");
+        fackto.withCourse("java-wise2021");
+        
+        ExerciseSubmitterManager manager = fackto.build();
+        
+        
+        Assignment assignment = new Assignment(SubmitterIT.homework02id,"Homework02", Assignment.State.SUBMISSION, true);
+        Submitter submitter = manager.getSubmitter(assignment);
+        
+        SubmissionResult result = submitter.submit(dir);
+        List<Problem> emptylist = new ArrayList<Problem>();
+        SubmissionResult resultTest = new SubmissionResult(true,emptylist);
+        
+        assertEquals(result, resultTest);
+        
+        });
+        
+      
+        
+    }
+
+}
