8eca857dc51377517961fed13c18198075dcd7da
Lukas Dönges <lukasdoenges@t-online.de>
2021-09-04 17:11:37 +0200

    deleted not used import

--- src/test/java/net/ssehub/teaching/exercise_submitter/lib/submission/SubmitterTest.java
+++ src/test/java/net/ssehub/teaching/exercise_submitter/lib/submission/SubmitterTest.java
@@ -1,41 +1,34 @@
 package net.ssehub.teaching.exercise_submitter.lib.submission;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.File;
 
 import org.junit.jupiter.api.Test;
 
-import com.squareup.okhttp.internal.http.CacheStrategy.Factory;
-
 import net.ssehub.teaching.exercise_submitter.lib.ExerciseSubmitterFactory;
 import net.ssehub.teaching.exercise_submitter.lib.ExerciseSubmitterManager;
-import net.ssehub.teaching.exercise_submitter.lib.ExerciseSubmitterManager.Credentials;
-import net.ssehub.teaching.exercise_submitter.lib.ExerciseSubmitterManagerTest;
 import net.ssehub.teaching.exercise_submitter.lib.data.Assignment;
 import net.ssehub.teaching.exercise_submitter.lib.data.Assignment.State;
-import net.ssehub.teaching.exercise_submitter.lib.data.Course;
 
 public class SubmitterTest {
-   @Test
+    @Test
     public void noDirectory() {
-       
-       ExerciseSubmitterFactory factory = new ExerciseSubmitterFactory();
-           factory.withDummyApiConnection()
-                  .withAuthUrl("localhost:5555")
-                  .withMgmtUrl("localhost:5555")
-                  .withSvnUrl("localhost:5555")
-                  .withUsername("username")
-                  .withPassword("username")
-                  .withCourse("java-wise2021");
-           
-           
+
+        ExerciseSubmitterFactory factory = new ExerciseSubmitterFactory();
+        factory.withDummyApiConnection()
+               .withAuthUrl("localhost:5555")
+               .withMgmtUrl("localhost:5555")
+               .withSvnUrl("localhost:5555")
+               .withUsername("username")
+               .withPassword("username")
+               .withCourse("java-wise2021");
+
         assertThrows(IllegalArgumentException.class, () -> {
-           ExerciseSubmitterManager manager = factory.build();
-           Submitter submitter = manager.getSubmitter(new Assignment("002","Homework02",State.IN_REVIEW,true));
-           submitter.submit(new File("main.java"));
+            ExerciseSubmitterManager manager = factory.build();
+            Submitter submitter = manager.getSubmitter(new Assignment("002", "Homework02", State.IN_REVIEW, true));
+            submitter.submit(new File("main.java"));
         });
     }
-    
+
 }
