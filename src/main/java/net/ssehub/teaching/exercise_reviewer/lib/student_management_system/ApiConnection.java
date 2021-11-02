9a25f86928c38f4b8b50e0c7b0bad36078faec29
Lukas Dönges <lukasdoenges@t-online.de>
2021-11-02 16:09:30 +0100

    adding new apiconnection function

--- src/main/java/net/ssehub/teaching/exercise_reviewer/lib/student_management_system/ApiConnection.java
+++ src/main/java/net/ssehub/teaching/exercise_reviewer/lib/student_management_system/ApiConnection.java
@@ -3,27 +3,36 @@ package net.ssehub.teaching.exercise_reviewer.lib.student_management_system;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
 import net.ssehub.studentmgmt.backend_api.ApiClient;
+import net.ssehub.studentmgmt.backend_api.api.AssessmentApi;
+import net.ssehub.studentmgmt.backend_api.api.AssignmentApi;
 import net.ssehub.studentmgmt.backend_api.api.AuthenticationApi;
 import net.ssehub.studentmgmt.backend_api.api.SubmissionApi;
+import net.ssehub.studentmgmt.backend_api.model.AssessmentDto;
 import net.ssehub.studentmgmt.backend_api.model.SubmissionDto;
 import net.ssehub.studentmgmt.backend_api.model.UserDto;
+import net.ssehub.studentmgmt.backend_api.model.AssignmentDto.CollaborationEnum;
 import net.ssehub.studentmgmt.sparkyservice_api.api.AuthControllerApi;
 import net.ssehub.studentmgmt.sparkyservice_api.model.AuthenticationInfoDto;
 import net.ssehub.studentmgmt.sparkyservice_api.model.CredentialsDto;
-import net.ssehub.teaching.exercise_reviewer.lib.data.Submission;
+import net.ssehub.teaching.exercise_reviewer.lib.data.Assessment;
 import net.ssehub.teaching.exercise_submitter.lib.data.Assignment;
+import net.ssehub.teaching.exercise_submitter.lib.data.Course;
+import net.ssehub.teaching.exercise_submitter.lib.data.Assignment.State;
 import net.ssehub.teaching.exercise_submitter.lib.student_management_system.ApiException;
 import net.ssehub.teaching.exercise_submitter.lib.student_management_system.AuthenticationException;
 import net.ssehub.teaching.exercise_submitter.lib.student_management_system.NetworkException;
+import net.ssehub.teaching.exercise_submitter.lib.student_management_system.UserNotInCourseException;
 
 /**
  * Handles the communication between the plugin and student management.
+ *
  * @author lukas
  *
  */
@@ -52,16 +61,17 @@ public class ApiConnection {
         this.mgmtClient = new ApiClient();
         this.mgmtClient.setBasePath(mgmtUrl);
     }
-    
+
     /**
-     * Logs into the system. Must be called before any other method of this connection.
-     * 
+     * Logs into the system. Must be called before any other method of this
+     * connection.
+     *
      * @param username The username.
      * @param password The password.
-     * 
-     * @throws NetworkException If the network communication fails.
+     *
+     * @throws NetworkException        If the network communication fails.
      * @throws AuthenticationException If the authentication fails.
-     * @throws ApiException If a generic exception occurs.
+     * @throws ApiException            If a generic exception occurs.
      */
     public void login(String username, String password) throws NetworkException, AuthenticationException, ApiException {
         AuthControllerApi api = new AuthControllerApi(this.authClient);
@@ -76,58 +86,117 @@ public class ApiConnection {
 
         } catch (net.ssehub.studentmgmt.sparkyservice_api.ApiException e) {
             if (e.getCode() == 401) {
-                throw new AuthenticationException("Invalid credentials: " + parseResponseMessage(e.getResponseBody()));
+                throw new AuthenticationException(
+                        "Invalid credentials: " + this.parseResponseMessage(e.getResponseBody()));
             }
-            throw handleAuthException(e);
+            throw this.handleAuthException(e);
 
         } catch (JsonParseException e) {
             throw new ApiException("Invalid JSON response", e);
         }
 
-        AuthenticationApi mgmtAuth = new AuthenticationApi(mgmtClient);
+        AuthenticationApi mgmtAuth = new AuthenticationApi(this.mgmtClient);
         try {
             this.loggedInUser = mgmtAuth.whoAmI();
 
         } catch (net.ssehub.studentmgmt.backend_api.ApiException e) {
-            throw handleMgmtException(e);
+            throw this.handleMgmtException(e);
 
         } catch (JsonParseException e) {
             throw new ApiException("Invalid JSON response", e);
         }
     }
+
     /**
      * Gets all reviewable submission from an assignment.
+     *
      * @param courseId
      * @param assignment
      * @return List<Submission>
      * @throws ApiException
      */
-    public List<Submission> getAllSubmissionFromAssignment(String courseId, Assignment assignment) throws ApiException {
-        SubmissionApi api = new SubmissionApi(this.mgmtClient);
-        List<Submission> submissions = null;
+    public List<Assessment> getAllAssessmentsFromAssignment(String courseId, Assignment assignment) 
+            throws ApiException {
+        AssessmentApi api = new AssessmentApi(this.mgmtClient);
+        AssessmentDto dto;
+        
+        List<Assessment> assessments = null;
+        try {
+
+            List<AssessmentDto> assessmentDto = api.getAssessmentsForAssignment(courseId, assignment.getManagementId(),
+                    null, null, null, null, null, null, null);
+
+            assessments = new ArrayList<Assessment>();
+
+            for (AssessmentDto element : assessmentDto) {
+                Assessment assessment = new Assessment(element.getId(), assignment, false);
+                assessments.add(assessment);
+            }
+
+        } catch (net.ssehub.studentmgmt.backend_api.ApiException e) {
+            throw this.handleMgmtException(e);
+        }
+
+        return assessments;
+    }
+    /**
+     * Retrieves all assignments created in the stuMgmt.
+     * @param course
+     * @return List<Assignments>
+     * @throws NetworkException
+     * @throws AuthenticationException
+     * @throws UserNotInCourseException
+     * @throws ApiException
+     */
+    public List<Assignment> getAssignments(Course course)
+            throws NetworkException, AuthenticationException, UserNotInCourseException, ApiException {
+        
+        AssignmentApi api = new AssignmentApi(mgmtClient);
+        List<Assignment> assignments;
         try {
             
-            List<SubmissionDto> submissionsDto = api.getAllSubmissions(courseId, null, null, null,
-                    assignment.getManagementId(), null, null, null);
-            
-            submissions = new ArrayList<Submission>();
-            
-            
-            for (SubmissionDto element : submissionsDto) {
-                Submission submission = new Submission(element.getAssignmentId(), element.getUserId(),
-                        element.getDisplayName(), element.getDate().toString())
-                        .withGroupId(element.getGroupId())
-                        .withGroupName(element.getGroupName());
-                
-                submissions.add(submission);
-            }
+            assignments = api.getAssignmentsOfCourse(course.getId()).stream()
+                    .map((assignment) -> {
+                        Assignment.State state;
+                        switch (assignment.getState()) {
+                        case EVALUATED:
+                            state = State.REVIEWED;
+                            break;
+                        case INVISIBLE:
+                            state = State.INVISIBLE;
+                            break;
+                        case IN_PROGRESS:
+                            state = State.SUBMISSION;
+                            break;
+                        case IN_REVIEW:
+                            state = State.IN_REVIEW;
+                            break;
+                        case CLOSED:
+                            state = State.CLOSED;
+                            break;
+                        default:
+                            state = State.INVISIBLE;
+                            break;
+                        }
+                        
+                        boolean groupwork = assignment.getCollaboration() != CollaborationEnum.SINGLE ? true : false;
+                        
+                        return new Assignment(assignment.getId(), assignment.getName(), state, groupwork);
+                    })
+                    .collect(Collectors.toList());
             
         } catch (net.ssehub.studentmgmt.backend_api.ApiException e) {
+            if (e.getCode() == 403) {
+                throw new UserNotInCourseException(parseResponseMessage(e.getResponseBody()));
+            }
             throw handleMgmtException(e);
+            
+        } catch (JsonParseException e) {
+            throw new ApiException("Invalid JSON response", e);
         }
-     
-        return submissions;
+        return assignments;
     }
+
 
     /**
      * Converts the given exception from the management API to a proper
@@ -139,9 +208,9 @@ public class ApiConnection {
      * <li>Code 401: {@link AuthenticationException} "Not logged in"</li>
      * <li>Fallback: {@link ApiException} "Unknown exception"</li>
      * </ul>
-     * 
+     *
      * @param exception The management exception.
-     * 
+     *
      * @return An {@link ApiException}.
      */
     private ApiException handleMgmtException(net.ssehub.studentmgmt.backend_api.ApiException exception) {
@@ -151,10 +220,11 @@ public class ApiConnection {
             result = new NetworkException(exception.getCause());
 
         } else if (exception.getCode() == 401) {
-            result = new AuthenticationException("Not logged in: " + parseResponseMessage(exception.getResponseBody()));
+            result = new AuthenticationException(
+                    "Not logged in: " + this.parseResponseMessage(exception.getResponseBody()));
 
         } else {
-            result = new ApiException("Unknown exception: " + parseResponseMessage(exception.getResponseBody()),
+            result = new ApiException("Unknown exception: " + this.parseResponseMessage(exception.getResponseBody()),
                     exception);
         }
 
@@ -170,9 +240,9 @@ public class ApiConnection {
      * <li>IOException: {@link NetworkException}</li>
      * <li>Fallback: {@link ApiException} "Unknown exception"</li>
      * </ul>
-     * 
+     *
      * @param exception The authentication exception.
-     * 
+     *
      * @return An {@link ApiException}.
      */
     private ApiException handleAuthException(net.ssehub.studentmgmt.sparkyservice_api.ApiException exception) {
@@ -182,7 +252,7 @@ public class ApiConnection {
             result = new NetworkException(exception.getCause());
 
         } else {
-            result = new ApiException("Unknown exception: " + parseResponseMessage(exception.getResponseBody()),
+            result = new ApiException("Unknown exception: " + this.parseResponseMessage(exception.getResponseBody()),
                     exception);
         }
 
@@ -193,9 +263,9 @@ public class ApiConnection {
      * Converts the JSON response message object into a simple message string. Uses
      * the <code>message</code> element in the given JSON object. As a fallback
      * (e.g. if JSON is not parseable), returns the whole response body.
-     * 
+     *
      * @param responseBody The response body of a failed API request.
-     * 
+     *
      * @return The message of the JSON result, or the whole response body.
      */
     private String parseResponseMessage(String responseBody) {
--- src/test/java/net/ssehub/teaching/exercise_reviewer/lib/student_management_system/ApiConnectionIT.java
+++ src/test/java/net/ssehub/teaching/exercise_reviewer/lib/student_management_system/ApiConnectionIT.java
@@ -22,7 +22,7 @@ import net.ssehub.teaching.exercise_submitter.lib.ExerciseSubmitterFactory;
 import net.ssehub.teaching.exercise_submitter.lib.ExerciseSubmitterManager;
 import net.ssehub.teaching.exercise_submitter.lib.data.Assignment;
 import net.ssehub.teaching.exercise_submitter.lib.data.Course;
-import net.ssehub.teaching.exercise_reviewer.lib.data.Submission;
+import net.ssehub.teaching.exercise_reviewer.lib.data.Assessment;
 import net.ssehub.teaching.exercise_reviewer.lib.student_management_system.ApiConnection;
 import net.ssehub.teaching.exercise_submitter.lib.student_management_system.ApiException;
 import net.ssehub.teaching.exercise_submitter.lib.student_management_system.GroupNotFoundException;
@@ -43,14 +43,14 @@ public class ApiConnectionIT {
     @BeforeAll
     public static void startServer() {
         docker = new StuMgmtDocker();
-        docker.createUser("svn", "abcdefgh");
         docker.createUser("adam", "123456");
         docker.createUser("student1", "123456");
         docker.createUser("student2", "123456");
         docker.createUser("student3", "123456");
         docker.createUser("student4", "123456");
 
-        courseId = docker.createCourse("java", "wise2021", "Programmierpraktikum: Java", "adam", "svn");
+        courseId = docker.createCourse("java", "wise2021", "Programmierpraktikum: Java", "adam");
+        docker.enableExerciseSubmissionServer(courseId);
 
         docker.enrollStudent(courseId, "student1");
         docker.enrollStudent(courseId, "student2");
@@ -61,7 +61,7 @@ public class ApiConnectionIT {
         docker.createGroup(courseId, "JP002", "student2", "student4");
 
         assignmentids.put("Homework01",
-                docker.createAssignment(courseId, "Homework01", AssignmentState.INVISIBLE, Collaboration.SINGLE));
+                docker.createAssignment(courseId, "Homework01", AssignmentState.INVISIBLE, Collaboration.GROUP));
         
         
         // start svn
@@ -72,12 +72,15 @@ public class ApiConnectionIT {
         
         //make test submissions
         
-        for (int i = 1; i <= 4; i++) {
-            AtomicInteger integer = new AtomicInteger(i);
-            assertDoesNotThrow(() -> submit(new File(TESTDATA, "works"),
+       
+        assertDoesNotThrow(() -> submit(new File(TESTDATA, "works"),
                     new Assignment(ApiConnectionIT.assignmentids.get("Homework01"), "Homework01",
-                    Assignment.State.SUBMISSION, false), "student" + Integer.toString(integer.get())));
-        }
+                    Assignment.State.SUBMISSION, true), "student1"));
+        
+        assertDoesNotThrow(() -> submit(new File(TESTDATA, "works"),
+                new Assignment(ApiConnectionIT.assignmentids.get("Homework01"), "Homework01",
+                Assignment.State.SUBMISSION, true), "student2"));
+
         
         
     }
@@ -88,7 +91,7 @@ public class ApiConnectionIT {
         ExerciseSubmitterFactory fackto = new ExerciseSubmitterFactory();
         fackto.withAuthUrl(docker.getAuthUrl());
         fackto.withMgmtUrl(docker.getStuMgmtUrl());
-      //  fackto.withSvnUrl(docker.getSvnUrl());
+        fackto.withExerciseSubmitterServerUrl(docker.getExerciseSubmitterServerUrl());
         fackto.withUsername(username);
         fackto.withPassword("123456");
         fackto.withCourse("java-wise2021");
@@ -104,23 +107,20 @@ public class ApiConnectionIT {
     public static void stopServer() {
         docker.close();
     }
-    @Disabled
+    
     @Test
-    public void getAllSubmissionFromAssignmentNoGroupTest() {
+    public void getAllSubmissionFromAssignmentGroupTest() {
         
         ApiConnection api = new ApiConnection(docker.getAuthUrl(), docker.getStuMgmtUrl());
         assertDoesNotThrow(() -> api.login("adam", "123456"));
         
-        List<Submission> submissions =  assertDoesNotThrow(() -> 
-            api.getAllSubmissionFromAssignment(courseId, new Assignment(
+        List<Assessment> assessments =  assertDoesNotThrow(() -> 
+            api.getAllAssessmentsFromAssignment(courseId, new Assignment(
                 ApiConnectionIT.assignmentids.get("Homework01"), "Homework01",
-               Assignment.State.SUBMISSION, false)));
+               Assignment.State.SUBMISSION, true)));
        
-        assertTrue(submissions.size() == 4);
+        assertTrue(assessments.size() == 2);
        
-        
-       
-        
         
     }
 }
